ff7d295c2afc5aaa662e87f1d2c6a83600ee09e6
Juwew1 <fauzyjunior53@gmail.com>
2025-10-23 16:38:53 +0700

    Menambahkan File dan Code CinemaMahasiswa dan Memperbaiki Code Main

--- Modul_2/src/Latihan_OCP/Main.java
+++ Modul_2/src/Latihan_OCP/Main.java
@@ -2,8 +2,8 @@ package Latihan_OCP;
 
 public class Main {
     public static void main(String[] args) {
-        StandardCinema standardCinema = new StandardCinema(100.0);
-        Double adminFee = standardCinema.calculateAdminFee();
+        CinemaMahasiswa cinemaMahasiswa = new CinemaMahasiswa(100.0);
+        Double adminFee = cinemaMahasiswa.calculateAdminFee();
         System.out.println(adminFee);
     }
 
