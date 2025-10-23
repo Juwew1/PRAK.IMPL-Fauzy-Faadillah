405d7109e264b86421a29a3bfaf7c2e791c7812b
Juwew1 <fauzyjunior53@gmail.com>
2025-10-23 16:14:50 +0700

    Ini adalah File dan Code Sebelum menggunakan prinsip LSP

---
+++ Modul_2/src/Latihan_LSP/Smartphone.java
@@ -0,0 +1,15 @@
+package Latihan_LSP;
+
+import java.util.Date;
+
+public class Smartphone extends Product {
+    @Override
+    String getName(){
+        return "Samsung S10+ Limited Edition";
+    }
+
+    @Override
+    Date getExpiredDate(){
+        return new Date();
+    }
+}
