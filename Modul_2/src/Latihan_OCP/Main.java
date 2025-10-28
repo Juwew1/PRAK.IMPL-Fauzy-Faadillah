1fa22af63775785d52b0f9d084d75008b57fa015
Juwew1 <fauzyjunior53@gmail.com>
2025-10-28 19:36:07 +0700

    Bug Fixed

--- Modul_2/src/Latihan_OCP/Main.java
+++ Modul_2/src/Latihan_OCP/Main.java
@@ -2,13 +2,10 @@ package Latihan_OCP;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         StandardCinema standardCinema = new StandardCinema(100.0);
         Double adminFee = standardCinema.calculateAdminFee();
-=======
         CinemaMahasiswa cinemaMahasiswa = new CinemaMahasiswa(100.0);
-        Double adminFee = cinemaMahasiswa.calculateAdminFee();
->>>>>>> Sesudah
+        adminFee = cinemaMahasiswa.calculateAdminFee();
         System.out.println(adminFee);
     }
 
