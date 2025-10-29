7c346594492f57162bb28024e6977cbeb6c5176d
Juwew1 <fauzyjunior53@gmail.com>
2025-10-29 10:54:49 +0700

    Menambahkan Code dan File Car,Motorcycle, VehicleInterface pada bagian Sebelum

---
+++ Modul_2/src/Latihan_ISP/Car.java
@@ -0,0 +1,25 @@
+package Latihan_ISP;
+
+    class Car implements VehicleInterface {
+
+        // Can be Implemented
+        @Override
+        public void drive() {
+
+        }
+
+        @Override
+        public void stop() {
+
+        }
+
+        @Override
+        public void refuel() {
+
+        }
+
+        @Override
+        public void openDoors() {
+
+        }
+    }
---
+++ Modul_2/src/Latihan_ISP/Motorcycle.java
@@ -0,0 +1,26 @@
+package Latihan_ISP;
+
+    class Motorcycle implements VehicleInterface{
+
+        // can be implemented
+        @Override
+        public void drive() {
+
+        }
+
+        @Override
+        public void stop() {
+
+        }
+
+        @Override
+        public void refuel() {
+
+        }
+
+        // can not be implemented
+        @Override
+        public void openDoors() {
+
+        }
+    }
